8932702d1f8e4b228ad1ed7f3f1d4578748b02ff
Ancherm <andrewcherm2014@gmail.com>
2023-04-13 13:43:34 +0400

    Функционал меню готов. Нужно сделать front

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -2,22 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] GameObject buttonContinue;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("OnGame") == true)
+        {
+            buttonContinue.SetActive(true);
+        }
+        else
+        {
+            buttonContinue.SetActive(false);
+        }
+    }
+    /*public void AppearToContinue()
+    {
+        if (PlayerPrefs.HasKey("OnGame") == true)
+        {
+            buttonContinue.SetActive(true);
+        }
+        else
+        {
+            buttonContinue.SetActive(false);
+        }
+    }*/
     public void ToPlay()
     {
-        SceneManager.LoadScene(0);
+        /*SceneManager.LoadScene(0);*/
+        PlayerPrefs.SetInt("OnGame", 1);
+        
     }
 
     public void ToContinue()
     {
         //TODO
+        PlayerPrefs.DeleteKey("OnGame");
     }
-    public void ToSettings()
-    {
-        //TODO
-    }
+    
     public void ToExit()
     {
         Application.Quit();
