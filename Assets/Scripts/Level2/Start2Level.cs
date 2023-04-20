a6be59a1edddb40dae7fd251dc37a15a3669d0db
Ancherm <andrewcherm2014@gmail.com>
2023-04-20 12:28:08 +0400

    Сделал 1 комнату Второго уровня(нужно прикрепить скрипт к дощечке

---
+++ Assets/Scripts/Level2/Start2Level.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start2Level : MonoBehaviour
+{
+    public static Vector3 PositionStart;
+    void Start()
+    {
+        PositionStart = transform.position;
+    }
+
+    
+}
