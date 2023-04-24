b34d5073e83140adc80efc671b65b3774c262ef2
Ancherm <andrewcherm2014@gmail.com>
2023-04-24 15:46:51 +0400

    Перенос

---
+++ Assets/StarfieldMaterials/Scripts/Move.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+	void Update()
+	{
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, 200, transform.position.z), -0.05f);
+
+    }
+}
